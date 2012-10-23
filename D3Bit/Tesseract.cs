f69cb6fe0023d0e39bf9afed370ec74ec9977361
Tanner Donovan <ttdonovan@gmail.com>
2012-10-22 21:26:27 -0700

    D3BitMacGUI Release build

--- D3Bit/Tesseract.cs
+++ D3Bit/Tesseract.cs
@@ -6,6 +6,7 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 
 namespace D3Bit
 {
@@ -20,14 +21,15 @@ namespace D3Bit
 
         public static string GetTextFromBitmap(Bitmap bitmap, string extraParams)
         {
-            bitmap.Save(Path.Combine("tesseract", "x.png"), ImageFormat.Png);
+            string pwd = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            bitmap.Save(Path.Combine(pwd, "tesseract", "x.png"), ImageFormat.Png);
 //          FIXME: find a way a better way to call tesseract
 //          $ ln -f /usr/local/bin/tesseract D3BitTest/bin/Debug/tesseract/tesseract_mac
-            ProcessStartInfo info = new ProcessStartInfo(Path.Combine("tesseract", "tesseract_mac"), string.Format("{0} {1} -l {2} {3}", Path.Combine("tesseract", "x.png"), Path.Combine("tesseract", "x"), language_code, extraParams));
+            ProcessStartInfo info = new ProcessStartInfo(Path.Combine(pwd, "tesseract", "tesseract_mac"), string.Format("{0} {1} -l {2} {3}", Path.Combine(pwd, "tesseract", "x.png"), Path.Combine(pwd, "tesseract", "x"), language_code, extraParams));
             info.WindowStyle = ProcessWindowStyle.Hidden;
             Process p = Process.Start(info);
             p.WaitForExit();
-            TextReader tr = new StreamReader(Path.Combine("tesseract", "x.txt"));
+            TextReader tr = new StreamReader(Path.Combine(pwd, "tesseract", "x.txt"));
             string res = tr.ReadToEnd();
             tr.Close();
             return res.Trim();
